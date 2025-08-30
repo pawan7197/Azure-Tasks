1b3267d05b46ee78b24e727c84033f8d8f06311b
pawan7197 <kothapallipawan7@gmail.com>
2025-08-30 16:09:33 +0530

    added

---
+++ Load-balancer/userdata.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo apt -y update
+sudo apt -y install nginx
+sudo systemctl start nginx
+sudo systemctl enable nginx
+echo "git installed"
+sudo rm -rf /var/www/html*
+sudo git clone https://github.com/Ai-TechNov/mario.git /var/www/html/
